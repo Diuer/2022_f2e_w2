16b295c7c38ac9c7fefff63bb13055b40e0df5b6
Diuer <diuer71@gmail.com>
2022-11-18 21:47:12 +0800

    style: rename

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
-
 import "./App.scss";
 
-import Header from "./sections/header";
+import Header from "./sections/Header";
 import Home from "./sections/Home";
 
 function App() {
